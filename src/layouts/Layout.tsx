e77e269bf87112679c8b1bf9fde1f9dbfa34a501
K <k.rev.urtica@gmail.com>
2022-07-31 23:05:13 +0900

    ♻️ refactor: use script component

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,3 +1,4 @@
+import { Script } from 'gatsby';
 import Footer from '../components/Footer';
 import HeaderBar from '../components/HeaderBar';
 
@@ -6,15 +7,22 @@ type Props = {
 };
 
 const Layout = ({ children }: Props) => (
-  <div className="font-body antialiased">
-    <HeaderBar />
+  <>
+    <Script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    />
 
-    <main className="container mx-auto max-w-screen-xl px-4 pt-12 pb-16">
-      {children}
-    </main>
+    <div className="font-body antialiased">
+      <HeaderBar />
 
-    <Footer />
-  </div>
+      <main className="container mx-auto max-w-screen-xl px-4 pt-12 pb-16">
+        {children}
+      </main>
+
+      <Footer />
+    </div>
+  </>
 );
 
 export default Layout;
